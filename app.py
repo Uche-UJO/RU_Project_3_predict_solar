e55dcbfc25040d7822c67e029be329d6eac7e395
Uche Obianyimuo <uutchei@gmail.com>
2020-07-24 21:32:45 -0500

    file updates

--- app.py
+++ app.py
@@ -100,3 +100,48 @@ def api_all_data():
 if __name__ == "__main__":
     app.run(debug=True)
 
+
+
+
+
+# ---------------------------------------------
+
+from flask import Flask, render_template, redirect, request
+from xgboost import XGBClassifier
+import pandas as pd
+import numpy as np
+import pickle
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+with open(f'best_xgb_model.pickle', "rb") as f:
+    model = pickle.load(f)
+
+feature_names = model.get_booster().feature_names
+
+# Route to render index.html template using data from Mongo
+@app.route("/", methods=["GET", "POST"])
+def home():
+    output_message = ""
+
+    if request.method == "POST":
+        recency = float(request.form["entity"])
+        frequency = float(request.form["time"])
+        
+
+        # data must be converted to df with matching feature names before predict
+        data = pd.DataFrame(np.array([[code, entity, solar_capacity_gwh, year]]), columns=feature_names)
+        result = model.predict(data)
+        if result == 1:
+            output_message = "Sunny Side Up!"
+        else:
+            output_message = "More to Come!"
+    
+    return render_template("home.html", message = output_message)
+
+if __name__ == "__main__":
+    app.run()
+
+
+
